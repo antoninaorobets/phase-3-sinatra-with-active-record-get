65a7b3c350016ae7d6b0e67b32b84c215c473fbd
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-21 15:18:00 -0400

    commit tree 7650d14d5a8552b10c1b93551636eaf282e87b04

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, "application/json"
   get '/' do
     { message: "Hello world" }.to_json
+  end
+  get '/games' do
+    # get all the games from the database
+    # return a JSON response with an array of all the game data
+    Game.all.to_json
+  end
+  get '/games/:id' do
+    id = params[:id]
+    game = Game.find_by(id: id)
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
